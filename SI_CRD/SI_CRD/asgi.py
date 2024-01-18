e9f2f9b583a30ad4a601300cf37dd03fd0090a49
Frankdvop <franklin.aguirre@correounivalle.edu.co>
2024-01-18 00:06:39 -0500

    first commit

---
+++ SI_CRD/SI_CRD/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for SI_CRD project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SI_CRD.settings')
+
+application = get_asgi_application()
