3625c7438ead16482a58641375fdc67739a47291
unknown <franklin.aguirre@correounivalle.edu.co>
2024-01-18 21:25:19 -0500

    creacion de algunos modelos

--- SI_CRD/login/models.py
+++ SI_CRD/login/models.py
@@ -2,4 +2,27 @@ from django.db import models
 from datetime import datetime
 # Create your models here.
 
+class campus (models.Model):
+    name=models.CharField(max_length=150,verbose_name='Nombre')
+    id=models.CharField(max_length=15,unique=True,verbose_name='Identificacion',primary_key=True)
+
+    
+
+class facultades (models.Model):
+    name=models.CharField(max_length=150,verbose_name='Nombre')
+    id=models.CharField(max_length=15,unique=True,verbose_name='Identificacion',primary_key=True)
+    campus=models.ForeignKey(campus, on_delete=models.PROTECT)
+
+class programas_academicos (models.Model):
+    name=models.CharField(max_length=150,verbose_name='Nombre')
+    id=models.CharField(max_length=4,unique=True,verbose_name='Codigo Programa',primary_key=True)
+    facultad=models.ForeignKey(facultades, on_delete=models.PROTECT)
+
+class escenario_deportivo (models.Model):
+    name=models.CharField(max_length=150,verbose_name='Nombre')
+    id=models.CharField(max_length=15,unique=True,verbose_name='Identificacion',primary_key=True)
+    estado=models.CharField(max_length=20,verbose_name='Estado')
+    campus=models.ForeignKey(campus, on_delete=models.PROTECT)
+    asistencia= models
+
 
